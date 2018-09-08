0050ebe96a211e10e3f123880f4dce8c6e55c9f1
saigowthamr <saigowthamr@gmail.com>
2018-09-09 05:23:06 +0530

    comming soon page

---
+++ src/components/Bio.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+
+
+class Bio extends React.Component {
+  render() {
+    return (
+      <div
+        style={{
+          display: 'flex',
+        }}
+      >
+
+        <p>
+         coming soon
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Bio
---
+++ src/pages/index.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { Link, graphql } from 'gatsby'
+import get from 'lodash/get'
+import Helmet from 'react-helmet'
+
+import Bio from '../components/Bio'
+import Layout from '../components/layout'
+
+
+class BlogIndex extends React.Component {
+  render() {
+    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
+    const siteDescription = get(
+      this,
+      'props.data.site.siteMetadata.description'
+    )
+
+
+    return (
+      <Layout location={this.props.location}>
+        <Helmet
+          htmlAttributes={{ lang: 'en' }}
+          meta={[{ name: 'description', content: siteDescription }]}
+          title={siteTitle}
+        />
+
+      </Layout>
+    )
+  }
+}
+
+export default BlogIndex
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "DD MMMM, YYYY")
+            title
+          }
+        }
+      }
+    }
+  }
+`
